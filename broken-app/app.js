590cce2fd8aea58976b982202f52703720ec9abb
Larry Volz <imaginologist@gmail.com>
2021-11-08 19:15:00 -0500

    almost working except for errors

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,20 +1,75 @@
 const express = require('express');
+const ExpressError = require("./expressError")
 let axios = require('axios');
 var app = express();
 
+app.use(express.json());  //to use JSON
+
+async function getDevelopers(userReq) {
+  let developerArr = [];
+  for (developer of userReq){
+    try{
+      let developerData = await axios.get(`https://api.github.com/users/${developer}`);
+      console.log("DEVELOPER DATA", developerData);
+    }catch(err){
+      next(err);
+    }
+    developerArr.push(developerData);
+    return developerArr;
+  }
+}
+//Given a list of GitHub users names, this should return information about those developers.
+//User POSTS requests a JSON body like {developers: [username, ...]}
+//in loop
+  //extract each username
+  //request from GitHub with https://api.github.com/users/${userName}
+  //store returned objects in array
+//Respond to user with contents of that array [ {name, bio}, ... ]
+//for example:   POST { "developers": ["joelburton", "elie"] }
+
+
+//It should have one route: POST /
 app.post('/', function(req, res, next) {
+
   try {
     //POST an array of developers (?) and make array of user data for each from github
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+    let userReq = req.body.developers;
+
+    let results = getDevelopers(userReq);
+
+    for (result in results) {
+      console.log(result);
+      res.json(results);
+    }
 
 
-    return res.send(JSON.stringify(out));
-  } catch {
+    // let results = req.body.developers.map(async d => {  
+    //   return await axios.get(`https://api.github.com/users/${d}`);
+    // }).then(data => console.log(data);)
+
+
+    // return res.send(JSON.stringify(out));
+  } catch(err) {
+    console.log(`CAUGHT ERROR: ${err} after attempted POST request`)
+    //TODO: Create a use() error catch-all for next to go to
     next(err);
   }
 });
 
-app.listen(3000);
+
+// 404 handler
+app.use(function (req, res, next) {
+  const notFoundError = new ExpressError("Not Found", 404);
+  return next(notFoundError)
+});
+
+/** general error handler */
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err.message,
+  });
+});
+
+app.listen(3000); 
--- broken-app/issues.md
+++ broken-app/issues.md
@@ -1,4 +1,10 @@
 # Broken App Issues
-- need axios installed
-- line 12: return res.send(JSON.stringify(out));  incorrect -> change to res.json to output
-- 
+- needed axios installed/required
+- line 12: return res.send(JSON.stringify(out));  incorrect? -> change to res.json to output
+- line 20: in catch block there was no err. Changed catch{} to catch(err){}
+- the app was not parsing JSON and so was not getting in the POST data at all.  So I added the line: 
+```app.use(express.json());```
+- When I tried to use the .map strategy and test the input coming back with a console.log I got "req.body.developers.map(...).then is not a function after attempted POST request
+TypeError: req.body.developers.map(...).then is not a function"  So I re-factored it more simply so each part could be tested.
+- used res.json() instead of stringify
+- added a 404 handler
